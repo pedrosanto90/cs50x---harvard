f71dc332d2f15a38a401ebc6e2210817dc194dc1
pedrosanto90 <pedro.bb.90@gmail.com>
2022-11-23 19:00:47 +0000

    refactor uppercase.c file

--- week2-arrays/uppercase.c
+++ week2-arrays/uppercase.c
@@ -10,14 +10,7 @@ int main(void)
 
     for (int i = 0, n = strlen(s); i < n; i++)
         {
-            if (islower(s[i]))
-            {
-                printf("%c", toupper(s[i]) );
-            }
-            else 
-            {
-                printf("%c", s[i]);
-            }
+            printf("%c", toupper(s[i]) );
         }
     printf("\n");
 }
