fcd8beace43a6f2dd8eb51ec36b79a7fbf8780b6
pedrosanto90 <pedro.bb.90@gmail.com>
2022-12-01 10:21:35 +0000

    add caeser.c file

---
+++ week2-arrays/caesar/caesar.c
@@ -0,0 +1,16 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[]) 
+{
+    char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    
+    string input = get_string("Input: ");
+
+    string output;
+
+    for (int i = 0; i < strlen(input); i++)
+    {
+        
+    }
+}
