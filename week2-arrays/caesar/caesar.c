d7f0fb1a897c618511c15282532b5cef0f73fc8f
pedrosanto90 <pedro.bb.90@gmail.com>
2022-12-01 10:51:05 +0000

    add string.h lib

--- week2-arrays/caesar/caesar.c
+++ week2-arrays/caesar/caesar.c
@@ -1,16 +1,16 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
 
-int main(int argc, string argv[]) 
-{
-    char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-    
-    string input = get_string("Input: ");
+int main(int argc, string argv[]) {
+  char arr[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+                'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+                's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
-    string output;
+  string input = get_string("Input: ");
 
-    for (int i = 0; i < strlen(input); i++)
-    {
-        
-    }
+  string output;
+
+  for (int i = 0; i < strlen(input); i++) {
+  }
 }
