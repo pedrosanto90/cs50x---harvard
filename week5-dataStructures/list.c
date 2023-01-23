85985b629f0ee3fa2c9d822d63c5bee7713c06b9
pedrosanto90 <pedro.bb.90@gmail.com>
2023-01-23 20:59:14 +0000

    update list.c file

--- week5-dataStructures/list.c
+++ week5-dataStructures/list.c
@@ -1,38 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    int *list = malloc(3 * sizeof(int));
-    
-    if(list == NULL){
-        return 1;
+typedef struct node{
+    int number;
+    struct node *next;
+} node;
+
+int main(int argc, char *argv[]) {
+    node *list = NULL;
+
+    for(int i = 1; i < argc; i++) {
+        int number = atoi(argv[i]);
+
+        node *n = malloc(sizeof(node));
+
+        if(n == NULL) {
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
     }
-
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    // ...
-    
-    int *tmp = malloc(4 * sizeof(int));
-    if(tmp == NULL) {
-        free(list);
-        return 1;
-    }
-
-    for( int i = 0; i < 3; i++){
-        tmp[i] = list[i];
-    }
-
-    tmp[3] = 4;
-
-    free(list);
-    list = tmp;
-
-    for (int i = 0; i < 3; i++) {
-        printf("%i\n", list[i]);
+    node *ptr = list;
+    while(ptr != NULL) {
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
     }
     
-    free(list);
-    return 0;
+    ptr = list;
+    while(ptr != NULL) {
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
+    }
 }
